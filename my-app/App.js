a66cc5dfb40559958b1c558ba49277047a636ac3
Areeba-Sadaqat <47633@students.riphah.edu.pk>
2025-03-16 13:42:30 +0500

    week6:counter

--- my-app/App.js
+++ my-app/App.js
@@ -1,31 +1,28 @@
 import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+export default function counter(){
+    const [count, setCounb] = useState(0);
 
-  const incr = () => setCount(count + 2);
-  const decr = () => setCount(count - 2);
-  const reset = () => setCount(0);
-  const bgcolor = () => (count < 5 ? "red" : "purple");
+    const incr = () => setCount(count + 2);
+    const decr = () => setCount(count - 2);
+    const reset = () => setCount(0);
+    const bgcolor = () => (count<5 ? 'red' : 'purple');
 
-  return (
-    <View style={styles.container}>
-      <Text style={{ color: bgcolor() }}>Counter: {count}</Text>
-      <Button title="Increment" onPress={incr} />
-      <Button title="Decrement" onPress={decr} />
-      <Button title="Reset" onPress={reset} />
-      <StatusBar style="auto" />
-    </View>
-  );
+    return(
+        <View style = {StyleSheet.container}>
+            <Text style= {{color : bgcolor()}}>counter :</Text>
+            <Button title="Increment" onPress={incr}></Button>
+            <Button title="Decrement" onPress={decr}></Button>
+            <Button title="Reset" onPress={reset}></Button>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    container:{
+        backgroudColor: 'white'
+    },
 });
