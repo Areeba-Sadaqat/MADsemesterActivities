f8f9f6a26e18ab5dcc019ca0d59fa17cc494b5e6
Areeba-Sadaqat <47633@students.riphah.edu.pk>
2025-03-16 13:05:44 +0500

    week5:React-Native-Layout

--- my-app/App.js
+++ my-app/App.js
@@ -1,16 +1,31 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Header from "./components/Header"; // Import the Header component
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-const App = () => {
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const incr = () => setCount(count + 2);
+  const decr = () => setCount(count - 2);
+  const reset = () => setCount(0);
+  const bgcolor = () => (count < 5 ? "red" : "purple");
+
   return (
-    <View>
-      <Header />
-      <View style={{ padding: 20 }}>
-        <Text>Welcome to my React Native App!</Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={{ color: bgcolor() }}>Counter: {count}</Text>
+      <Button title="Increment" onPress={incr} />
+      <Button title="Decrement" onPress={decr} />
+      <Button title="Reset" onPress={reset} />
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
